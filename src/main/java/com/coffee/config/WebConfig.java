884656da4d22c1425d489c55f912eec0bae766b6
sw study <yalejong96@gmail.com>
2026-04-07 16:57:55 +0900

    2026/04/07

--- src/main/java/com/coffee/config/WebConfig.java
+++ src/main/java/com/coffee/config/WebConfig.java
@@ -2,22 +2,24 @@ package com.coffee.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration // 이 파일은 스프링에서 설정용 파일로 사용하겠습니다.
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
-/*    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // 3000번 포트에서 GET부터 PATCH까지의 열거한 요청들을 모두 수락하겠습니다.
-        registry.addMapping("/**") *//* 모든 경로 허용  *//*
-                .allowedOrigins("http://localhost:5173") *//* react 포트 *//*
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")  *//* 허용할 메소드 *//*
-                .allowCredentials(true) ; // 쿠키 전송 허용
-    }*/
+//    // Cors 설정을 위한 오버라이딩
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        // 3000번 포트에서 GET부터 PATCH까지의 열거한 요청들을 모두 수락하겠습니다.
+//        registry.addMapping("/**") /* 모든 경로 허용  */
+//                .allowedOrigins("http://localhost:5173") /* react 포트 */
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")  /* 허용할 메소드 */
+//                .allowCredentials(true) ; // 쿠키 전송 허용
+//    }
+
+    // 홈 페이지 구현 절차를 위한 코딩
     @Value("${uploadPath}")
-    private String uploadPath ;
+    private String uploadPath ; // file:///C:/shop/images/
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
